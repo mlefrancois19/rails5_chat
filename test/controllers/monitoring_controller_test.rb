b8950191f09f559b72b677e71f698fed2713cf9e
Mathieu LeFrançois <mlefrancois19@gmail.com>
2017-03-06 21:04:23 -0500

    remove monitoring stuff

--- test/controllers/monitoring_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MonitoringControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get monitoring_index_url
-    assert_response :success
-  end
-
-end
